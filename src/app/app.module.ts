03def548f299c8fce0d81e03d571454a60a8c328
mas250 <mataaronshaw@gmail.com>
2019-12-11 15:55:58 +0000

    components with data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+        EventsAppComponent,
+      EventsListComponent
   ],
   imports: [
     BrowserModule
