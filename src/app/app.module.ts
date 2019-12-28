8732035ef84267b34e410ad69b3d5dfab5ce76c7
mas250 <mataaronshaw@gmail.com>
2019-12-28 12:29:24 +0000

    navigation component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core'
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { NavBarComponent } from './nav/navbar.component'
 
 @NgModule({
   declarations: [
         EventsAppComponent,
         EventsListComponent,
-      EventThumbnailComponent
+        EventThumbnailComponent,
+        NavBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core'
                 <div>
                   <h1>Upcoming Angular Events</h1>
                   <hr/>
-                  <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></event-thumbnail>
+                  <event-thumbnail [event]="event1"></event-thumbnail>
                 </div>
              `
 })
@@ -25,9 +25,4 @@ export class EventsListComponent {
             country:'England'
         }
     }
-
-    handleEventClicked(data) {
-        console.log("recieved:", data)
-    }
-
 }
