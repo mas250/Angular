6862eff29162bfd0da8f9140941aa1fe5930e901
mas250 <mataaronshaw@gmail.com>
2020-01-06 22:25:46 +0000

    dependency injection for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { NavBarComponent } from './nav/navbar.component'
   imports: [
     BrowserModule
   ],
-
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
         <div class="well hoverwell thumbnail">
             <h2>{{ event.name }}</h2>
             <div> Date: {{ event?.date }}</div>
-            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+            <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
               Time: {{ event?.time }}           
               <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
               <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -25,8 +25,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
         </div>
       `,
     styles: [`
-              .bold { font-weight: bold; }
-              .green {color: #003300 !important; }
               .thumbnail {min-height: 210px; }
               .pad-left { margin-left: 10px; }
               .well div {color: #bbb; }
@@ -36,9 +34,10 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 export class EventThumbnailComponent {
     @Input() event: any
 
-    getStartTimeClass() {
-        const isEarlyStart = this.event && this.event.time === '8:00 am'
-        return { green: isEarlyStart, bold: isEarlyStart }
+    getStartTimeStyle():any {
+        if (this.event && this.event.time === '8:00 am')
+            return { color: '#003300', 'font-weight': 'bold' }
+        return {}
     }
 
 }
