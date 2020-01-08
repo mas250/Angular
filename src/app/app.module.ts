d253196edd5e693269a9438efda693fdbd475575
mas250 <mataaronshaw@gmail.com>
2020-01-08 14:12:00 +0000

    service injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { EventService } from './events/shared/event.service'
   imports: [
     BrowserModule
   ],
-  providers: [EventService],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service'
 
+declare let toastr
 
 @Component({
   selector: 'events-list',
@@ -10,7 +12,7 @@ import { EventService } from './shared/event.service';
                   <hr/>
                   <div class="row">
                     <div *ngFor="let event of events" class="col-md-5">
-                      <event-thumbnail [event]="event"></event-thumbnail>
+                      <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
                     </div>
                   </div>
                 </div>
@@ -20,12 +22,16 @@ import { EventService } from './shared/event.service';
 export class EventsListComponent {
     events: any[]
 
-    constructor(private eventService: EventService) {
+    constructor(private eventService: EventService, private toastr: ToastrService) {
 
         
     }
     ngOnInit() {
         this.events = this.eventService.getEvents()
-    }
+  }
+
+  handleThumbnailClick(eventName) {
+    toastr.success(eventName)
+  }
 
 }
